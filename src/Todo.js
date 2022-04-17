7bd75316b33d965948b53cff2842ee407886d789
Taneya <twil609@aucklanduni.ac.nz>
2022-04-17 11:18:59 +1000

    handle toggling checked and uuid

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
-export default function Todo( { todo } ) {
+export default function Todo( { todo, toggleTodo } ) {
+
+
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
+
   return (
     <div>
         {/* to print todos */}
         {/* include name reference from App.js */}
-        
+    
         
         <label>
-            <input type="checkbox" checked={todo.complete}/>
+            <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
             {todo.name}
         </label>
       
--- src/TodoList.js
+++ src/TodoList.js
@@ -15,7 +15,7 @@ export default function TodoList({ todos }) {
     // for each todo Component, return todo. Requires a Key-Prop so that React will only update the todos in the array that change/are new rather than rerendering all elements. Also need an object to store if the todo is complete and so need an ID in App.js as well as a Name.
     // make key same as id to make unique
     todos.map(todo => {
-        return <Todo key={todo.id} todo={todo } />
+        return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
     })
   )
 
